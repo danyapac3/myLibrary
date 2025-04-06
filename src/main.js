60a2d9c8715dbfdd1b6d17c22dde58132ccaf50d
danya.pac3 <danya.pac3@gmail.com>
2025-04-06 12:19:57 +0300

    Migrate project on sass

--- src/main.js
+++ src/main.js
@@ -1,24 +1,21 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+//common style
+import "./sass/common/fonts.sass";
+import "./sass/common/normalize.sass";
+import "./sass/common/general.sass";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+//layout style
+import "./sass/layout/header.sass";
+import "./sass/layout/page.sass";
 
-setupCounter(document.querySelector('#counter'))
+//page style
+import "./sass/pages/home.sass";
+
+//components style
+import "./sass/components/s-in-progress.sass";
+import "./sass/components/s-completed.sass";
+import "./sass/components/section-header.sass";
+import "./sass/components/book-in-progress.sass";
+import "./sass/components/book-completed.sass";
+import "./sass/components/counter.sass";
+import "./sass/components/input-group.sass";
+import "./sass/components/select.sass";
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
+import path from "path";
+const pathSrc = path.resolve(__dirname, "./src");
+
+
 export default {
-  
+  css: {
+    preprocessorOptions: {
+      sass: {
+        additionalData: `@use "${pathSrc}/sass/shared" as *\n`
+      }
+    }
+  }
 }
