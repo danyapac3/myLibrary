ea4b84f3ae065b3ae81234eede097e0b76e5610e
danya.pac3 <danya.pac3@gmail.com>
2025-04-06 21:48:20 +0300

    Implement search bar

--- src/main.js
+++ src/main.js
@@ -19,3 +19,4 @@ import "./sass/components/book-completed.sass";
 import "./sass/components/counter.sass";
 import "./sass/components/input-group.sass";
 import "./sass/components/select.sass";
+import "./sass/components/search.sass";
