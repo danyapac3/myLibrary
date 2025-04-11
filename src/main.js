8525f57257365b82ccf220600ee5461a58992ebe
danya.pac3 <danya.pac3@gmail.com>
2025-04-11 16:13:22 +0300

    Finish responsive  book-to-pick style

--- src/main.js
+++ src/main.js
@@ -12,18 +12,18 @@ import "./sass/pages/home.sass";
 
 //components style
 import "./sass/components/section-header.sass";
+import "./sass/components/counter.sass";
 import "./sass/components/m-pick-books.sass";
 import "./sass/components/s-in-progress.sass";
 import "./sass/components/s-completed.sass";
 import "./sass/components/book-in-progress.sass";
 import "./sass/components/book-completed.sass";
-import "./sass/components/book-to-pick.sass";
-import "./sass/components/counter.sass";
 import "./sass/components/info-group.sass";
 import "./sass/components/select.sass";
 import "./sass/components/search.sass";
 import "./sass/components/button.sass";
 import "./sass/components/toggle-button.sass";
+import "./sass/components/book-to-pick.sass";
 
 // ---Debug--- //
 const modal = document.querySelector('.modal-pick-books');
